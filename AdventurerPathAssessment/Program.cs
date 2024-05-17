182b5d1916ca1f05b47fcf9a0e86853797017846
brettjohnson22 <br.onson@outlook.com>
2024-05-17 15:39:53 -0500

    Removed comments.

--- AdventurerPathAssessment/Program.cs
+++ AdventurerPathAssessment/Program.cs
@@ -5,8 +5,6 @@
         static void Main(string[] args)
         {
             double totalDistance = Solution.FindSteps("15F6B6B5L16R8B16F20L6F13F11R");
-            //double totalDistance = Solution.FindSteps("5F5R");
-            //double totalDistance = Solution.FindSteps("hello");
             Console.WriteLine($"Euclidean distance is {totalDistance} steps.");
         }
     }
