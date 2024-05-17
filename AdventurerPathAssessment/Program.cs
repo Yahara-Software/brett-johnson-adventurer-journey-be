c0be9443c110e5ed045156b1f8e661b7d5c440a3
brettjohnson22 <br.onson@outlook.com>
2024-05-17 15:06:47 -0500

    Challenge complete.

---
+++ AdventurerPathAssessment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventurerPathAssessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalDistance = Solution.FindSteps("15F6B6B5L16R8B16F20L6F13F11R");
+            //double totalDistance = Solution.FindSteps("5F5R");
+            //double totalDistance = Solution.FindSteps("hello");
+            Console.WriteLine($"Euclidean distance is {totalDistance} steps.");
+        }
+    }
+}
---
+++ AdventurerPathAssessment/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventurerPathAssessment
+{
+    internal class Solution
+    {
+        public static double FindSteps(string directions)
+        {
+            char[] splitters = ['F', 'B', 'R', 'L'];
+            try
+            {
+                double[] stepCounts = directions.Split(splitters, StringSplitOptions.RemoveEmptyEntries).Select(s => double.Parse(s)).ToArray();
+
+                Dictionary<char, double> stepsDict = splitters.ToDictionary(c => c, c => 0.0);
+
+                int index = 0;
+                foreach (char character in directions)
+                {
+                    if (splitters.Contains(character))
+                    {
+                        stepsDict[character] += stepCounts[index];
+                        index++;
+                    }
+                }
+
+                double yAxis = stepsDict['F'] - stepsDict['B'];
+                double xAxis = stepsDict['R'] - stepsDict['L'];
+
+                double totalDistance = Math.Sqrt((yAxis * yAxis) + (xAxis * xAxis));
+
+                return totalDistance;
+            }
+            catch
+            {
+                throw new FormatException("Directions formatted incorrectly, cannot parse.");
+            }
+        }
+    }
+}
